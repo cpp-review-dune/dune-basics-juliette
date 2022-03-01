f8e5b9dcff5e94c3a5f0aa245abfa80fe6319a52
Juliette009 <krojasl@unal.edu.co>
2022-03-01 11:05:45 -0500

    Primer proyecto en DUNE

---
+++ primer-proyecto/config.h.cmake
@@ -0,0 +1,45 @@
+/* begin primer-proyecto
+   put the definitions for config.h specific to
+   your project here. Everything above will be
+   overwritten
+*/
+
+/* begin private */
+/* Name of package */
+#define PACKAGE "@DUNE_MOD_NAME@"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "@DUNE_MAINTAINER@"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "@DUNE_MOD_NAME@"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "@DUNE_MOD_NAME@ @DUNE_MOD_VERSION@"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "@DUNE_MOD_NAME@"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL "@DUNE_MOD_URL@"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "@DUNE_MOD_VERSION@"
+
+/* end private */
+
+/* Define to the version of primer-proyecto */
+#define PRIMER_PROYECTO_VERSION "@PRIMER_PROYECTO_VERSION@"
+
+/* Define to the major version of primer-proyecto */
+#define PRIMER_PROYECTO_VERSION_MAJOR @PRIMER_PROYECTO_VERSION_MAJOR@
+
+/* Define to the minor version of primer-proyecto */
+#define PRIMER_PROYECTO_VERSION_MINOR @PRIMER_PROYECTO_VERSION_MINOR@
+
+/* Define to the revision of primer-proyecto */
+#define PRIMER_PROYECTO_VERSION_REVISION @PRIMER_PROYECTO_VERSION_REVISION@
+
+/* end primer-proyecto
+   Everything below here will be overwritten
+*/
---
+++ primer-proyecto/dune/primer-proyecto/primer-proyecto.hh
@@ -0,0 +1,6 @@
+#ifndef PRIMER_PROYECTO_HH
+#define PRIMER_PROYECTO_HH
+
+// add your classes here
+
+#endif // PRIMER_PROYECTO_HH
